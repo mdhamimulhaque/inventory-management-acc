24b1c8f96255fe9c21906596576bf50303bf00a7
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-25 11:49:32 +0600

    basic server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+
+const app = require("./app");
+
+// server
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.yellow.bold);
+});
