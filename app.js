afeef30451283bbcf67d0e0ef4940445d83a5515
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-25 12:59:52 +0600

    created productSchema

--- app.js
+++ app.js
@@ -1,16 +1,70 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(cors());
 
-
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large"],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price can't be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "litre", "pcs"],
+        message: "unit value can't be [VALUE], must be kg/litre/pcs",
+      },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "quantity can't be negative"],
+      validate: {
+        validator: function (value) {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        message: (props) => `${props.value} is not an integer number!`,
+      },
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        message: "status can't be {VALUE}",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const colors = require("colors");
 
 const app = require("./app");
 
+// database connection
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+  console.log("Database Connected successful".red.bold);
+});
+
 // server
 const port = process.env.PORT || 8080;
 
